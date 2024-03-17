4acda07131b5bf062124a336e535036c5f906ce1
AhmedEmbedd <163743597+AhmedEmbedd@users.noreply.github.com>
2024-03-17 17:33:50 +0200

    Add files via upload

--- stats.h
+++ stats.h
@@ -1,1 +1,14 @@
+/******************************************
+****** Name   : Ahmed Abdulla Mourad ******
+****** Subject: Coursear Course      ******
+****** Task   : Assignement          ******
+****** File   : stats.h              ******
+******************************************/
 
+void          print_statistics(int * pointer);
+unsigned char print_array     (int * pointer,int index);
+unsigned char find_median     (int * pointer,int index);
+unsigned char find_mean       (int * pointer,int index);
+unsigned char find_maximum    (int * pointer,int index);
+unsigned char find_minimum    (int * pointer,int index);
+void          sort_array      (int * pointer);
